cd491edbaa44802972bdf6ab21f87a2d85ac21b0
ashr-aful007 <ashraful99980@gmail.com>
2022-09-14 12:22:45 +0600

    fixed display password and passowrd will show when user incrise number on time create

--- debugging-project-1-main/script.js
+++ debugging-project-1-main/script.js
@@ -11,6 +11,8 @@ const getElement = (id) => {
 const handleSlider = (event) => {
   const charCountEl = getElement("char-count");
   charCountEl.innerText = event;
+
+  generatePassword()
 };
 
 const handleCheckbox = () => {
